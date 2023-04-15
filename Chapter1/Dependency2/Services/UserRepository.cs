272cf2dc1d9b24099e5b8395766480f232e8505a
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-04-15 14:33:41 +0300

    rfct

--- Chapter1/Dependency2/Services/UserRepository.cs
+++ Chapter1/Dependency2/Services/UserRepository.cs
@@ -5,6 +5,10 @@ namespace ServiceImplementations
 {
     public class UserRepository : IUserRepository
     {
+        public UserRepository()
+        {
+            
+        }
         public IUser GetByID(int userID)
         {
             return new User() { Id = 456 };
