5b82ba49a9ae047e404f5fab8d66223306691e83
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-04-17 09:51:52 +0300

    without use aop

---
+++ Chapter1/AOPExample/AOPExample/AgeCalculator.cs
@@ -0,0 +1,27 @@
+﻿namespace AOPExample
+{
+    internal class AgeCalculator
+    {
+        public int Add(int a, int b)
+        {
+            if (a < 0 || b < 0)
+                throw new ArgumentOutOfRangeException("Age must be positive");
+
+            Console.WriteLine("Get ages for Add");
+            var answer = a + b;
+            Console.WriteLine("Calculation done");
+            return answer;
+        }
+
+        public int Sub(int a, int b)
+        {
+            if (a < 0 || b < 0)
+                throw new ArgumentOutOfRangeException("Age must be positive");
+
+            Console.WriteLine("Get ages for Add");
+            var answer = a - b;
+            Console.WriteLine("Calculation done");
+            return answer;
+        }
+    }
+}
---
+++ Chapter1/AOPExample/AOPExample/Program.cs
@@ -0,0 +1,5 @@
+﻿using AOPExample;
+
+AgeCalculator ageCalculator = new AgeCalculator();
+Console.WriteLine(ageCalculator.Add(5, 6));
+Console.ReadLine();
