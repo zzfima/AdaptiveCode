a194c6bbfc0e8776557bcc9fbd94aeea0b3c426b
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-04-17 12:17:05 +0300

    rfct

--- Chapter1/AOPExample/AOPExample/Program.cs
+++ Chapter1/AOPExample/AOPExample/Program.cs
@@ -3,11 +3,13 @@
 Console.WriteLine("No AOP");
 AgeCalculatorNoAOP ageCalculatorNoAOP = new AgeCalculatorNoAOP();
 Console.WriteLine(ageCalculatorNoAOP.Add(5, 6));
+Console.WriteLine(ageCalculatorNoAOP.Sub(5, 6));
 
 Console.WriteLine();
 
 Console.WriteLine("AOP");
 AgeCalculatorAOP ageCalculatorAOP = new AgeCalculatorAOP();
 Console.WriteLine(ageCalculatorAOP.Add(5, 6));
+Console.WriteLine(ageCalculatorAOP.Sub(5, 6));
 
 Console.ReadLine();
