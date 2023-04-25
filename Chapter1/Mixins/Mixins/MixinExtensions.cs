9fc9af49c7bf87f337dbe0f57493685482655e39
zzfima <zzfima@gmail.com>
2023-04-25 16:44:53 +0300

    start

---
+++ Chapter1/Mixins/Mixins/MixinExtensions.cs
@@ -0,0 +1,11 @@
+﻿public static class MixinExtensions
+{
+    public static void FirstExtensionMethod(this ITargetInterface target)
+    {
+        Console.WriteLine("The first extension method was called.");
+    }
+    public static void SecondExtensionMethod(this ITargetInterface target)
+    {
+        Console.WriteLine("The second extension method was called.");
+    }
+}
